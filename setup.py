d071c36d2aff56a564983d511b14b679d7f575d3
Chris Kamphuis <mail@chriskamphuis.com>
2022-09-14 18:27:56 +0200

    Add setup, and update gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
     name="mmead",
     version="0.0.1",
@@ -10,7 +7,6 @@ setuptools.setup(
     author_email="chris@cs.ru.nl",
     description="MS MARCO entity annotations and disambiguations",
     url="https://github.com/informagi/mmead",
-    install_requires=requirements,
     packages=setuptools.find_packages(),
     python_requires=">=3.6"
 )
