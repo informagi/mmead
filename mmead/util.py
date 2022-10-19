6cc68c74da0a344c56c0f329d2daf35fb036e551
Chris Kamphuis <mail@chriskamphuis.com>
2022-10-19 15:00:02 +0200

    Code to create and load an embeddings table
    Embeddings are represented using a LIST of REAL datatypes

--- mmead/util.py
+++ mmead/util.py
@@ -1,12 +1,60 @@
+import os
+import duckdb
+import json
+
 from .download_info import EMBEDDING_INFO, LINK_INFO, MAPPING_INFO
-from .retrieve import download_and_unpack
+from .retrieve import download_and_unpack, get_cache_home
+from .connection import get_connection
 
 
-def load_embeddings(key):
+def load_embeddings(key, db_path=os.path.join(get_cache_home(), 'mmead.db'), force=False, verbose=True):
     if key not in EMBEDDING_INFO:
         raise ValueError(f'{key} is not a valid embedding identifier')
-    path_to_data = download_and_unpack(key)
-    return path_to_data
+    path_to_data = ''
+    try:
+        path_to_data = download_and_unpack(key)
+        cursor = get_connection(db_path).cursor
+        try:
+            cursor.execute(f"SELECT * FROM {key} LIMIT 1")
+            if force:
+                if verbose:
+                    print(f"Table {key} already exists but force=True, so we drop the table and reload...")
+            else:
+                if verbose:
+                    print(f"Table {key} already exists, skipping the loading...")
+                return cursor
+        except duckdb.CatalogException:
+            pass
+        with open(path_to_data, 'rt') as embedding_file:
+            entries, dims = [int(e) for e in embedding_file.readline().strip().split()]
+            cursor.execute("START TRANSACTION")
+            cursor.execute(
+                f"CREATE OR REPLACE TABLE {key} (id INTEGER PRIMARY KEY, key VARCHAR, embedding REAL[])"
+            )
+            cursor.execute("CREATE TEMP SEQUENCE identifiers;")
+            for n in range(entries):
+                try:
+                    embedding = embedding_file.readline().strip().split()
+                    identifier, values = embedding[0], [float(e) for e in embedding[1:]]
+                    cursor.execute(f"INSERT INTO {key} VALUES (nextval('identifiers'), ?, {json.dumps(values)})", [identifier])
+                except EOFError:
+                    cursor.execute("ROLLBACK")
+                    cursor.execute("DROP SEQUENCE identifiers")
+                    raise ValueError("There is less data than the header suggests.")
+            try:
+                assert embedding_file.read() == ''
+            except AssertionError:
+                cursor.execute("ROLLBACK")
+                cursor.execute("DROP SEQUENCE identifiers")
+                raise ValueError("There is more data than the header suggests")
+            cursor.execute("COMMIT")
+            cursor.execute("DROP SEQUENCE identifiers")
+    finally:
+        if os.path.exists(path_to_data):
+            os.remove(path_to_data)
+    if verbose:
+        print(f"Table {key} is available...")
+    return cursor
 
 
 def load_mappings(key):
