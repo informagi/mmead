deb7bd8b01c8da152c9d654a38d93405c5115f55
Chris Kamphuis <mail@chriskamphuis.com>
2022-11-24 15:44:42 +0100

    Make get_links available

--- mmead/__init__.py
+++ mmead/__init__.py
@@ -1,4 +1,5 @@
 from .retrieve import download_and_unpack, get_cache_home
 from .util import load_links, load_mappings, load_embeddings
+from .data.links import get_links
 
-__all__ = ['download_and_unpack', 'load_links', 'load_mappings', 'load_embeddings']
+__all__ = ['download_and_unpack', 'load_links', 'load_mappings', 'load_embeddings', 'get_links']
