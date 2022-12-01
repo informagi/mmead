8156c0e9e2256473c7f03f77d2448a6b87d42af0
Chris Kamphuis <mail@chriskamphuis.com>
2022-12-01 17:15:57 +0100

    Update queries to output JSON v2 passages

--- mmead/data/links.py
+++ mmead/data/links.py
@@ -95,14 +95,35 @@ class V2PassageLinks(Links):
 
     def load_links_from_docid(self, docid):
         _, __, segment, offset = docid.split('_')
-        return self.load_links_from_segment_and_offset(segment, offset)
+        return self.load_links_from_segment_and_offset(docid, segment, offset)
 
-    def load_links_from_segment_and_offset(self, segment, offset):
+    def load_links_from_segment_and_offset(self, docid, segment, offset):
+        # self.cursor.execute(f"""
+        #     SELECT field, entity_id, start_pos, end_pos, entity, id
+        #     FROM {self.identifier}
+        #     WHERE segment = '{segment}'
+        #     AND passage_offset = '{offset}'
+        # """)
         self.cursor.execute(f"""
-            SELECT field, entity_id, start_pos, end_pos, entity, id
-            FROM {self.identifier}
-            WHERE segment = '{segment}'
-            AND passage_offset = '{offset}'
+            SELECT to_json(
+                {{
+                    'passage': json_group_array(x),
+                    'docid': '{docid}'
+                }}
+            )
+            FROM (
+                SELECT to_json(
+                    {{
+                        'entity_id': entity_id,
+                        'start_pos': start_pos,
+                        'end_pos': end_pos,
+                        'entity': entity 
+                    }}
+                ) as x
+                FROM {self.identifier}
+                WHERE segment = '{segment}'
+                AND passage_offset = '{offset}'
+            )
         """)
         return self.cursor.fetchone()[0]
 
