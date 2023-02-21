7fb6c358c6275844ac8f448ab9cfaa8cedf461d6
Chris Kamphuis <mail@chriskamphuis.com>
2023-02-21 16:31:24 +0100

    Support for blink links msmarco v1 passages

--- mmead/data/links.py
+++ mmead/data/links.py
@@ -2,9 +2,11 @@ from ..util import load_links
 import json
 
 
-def get_links(version, passage_or_doc, verbose=True):
-    if version == 'v1' and passage_or_doc == 'passage':
-        return V1PassageLinks(verbose)
+def get_links(version, passage_or_doc, linker='rel', verbose=True):
+    if version == 'v1' and passage_or_doc == 'passage' and linker == 'rel':
+        return V1PassageLinksREL(verbose)
+    elif version == 'v1' and passage_or_doc == 'passage' and linker == 'blink':
+        return V1PassageLinksBlink(verbose)
     elif version == 'v1' and passage_or_doc == 'doc':
         return V1DocLinks(verbose)
     elif version == 'v2' and passage_or_doc == 'passage':
@@ -17,9 +19,9 @@ def get_links(version, passage_or_doc, verbose=True):
 
 class Links:
 
-    def __init__(self, key, verbose=True):
+    def __init__(self, key, verbose=True, linker='rel'):
         self.identifier = key
-        self.cursor = load_links(self.identifier, verbose=verbose)
+        self.cursor = load_links(self.identifier, verbose=verbose, linker=linker)
 
     def load_links_from_docid(self, docid):
         raise NotImplementedError()
@@ -28,9 +30,40 @@ class Links:
         raise NotImplementedError()
 
 
-class V1PassageLinks(Links):
+class V1PassageLinksREL(Links):
     def __init__(self, verbose):
         super().__init__(key="msmarco_v1_passage_links", verbose=verbose)
+
+    def load_links_from_docid(self, docid):
+        self.cursor.execute(f"""
+            SELECT to_json(
+                {{
+                    'passage': json_group_array(x),
+                    'pid': '{docid}'
+                }}
+            )
+            FROM (
+                SELECT to_json(
+                    {{
+                        'entity_id': entity_id,
+                        'start_pos': start_pos,
+                        'end_pos': end_pos,
+                        'entity': entity,
+                    }}
+                ) AS x
+                FROM {self.identifier}
+                WHERE pid = {docid}
+            )
+        """)
+        return self.cursor.fetchone()[0]
+
+    def load_links_from_docids(self, docid):
+        raise NotImplementedError()
+
+
+class V1PassageLinksBlink(Links):
+    def __init__(self, verbose):
+        super().__init__(key="msmarco_v1_passage_links", verbose=verbose, linker='blink')
 
     def load_links_from_docid(self, docid):
         self.cursor.execute(f"""
