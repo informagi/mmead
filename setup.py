c39c567f5a810106d078dcbb1420487f556e0ece
Chris Kamphuis <mail@chriskamphuis.com>
2023-05-03 11:12:04 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mmead",
-    version="0.0.3",
+    version="0.1.0",
     author="Chris Kamphuis",
     author_email="chris@cs.ru.nl",
     description="MS MARCO entity annotations and disambiguations",
